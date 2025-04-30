1cede24930cf1e446f52a9037262a2baaac7ce7a
onkarbhawar111 <onkarbhawar111@gmail.com>
2025-04-30 11:57:31 +0530

    30-4

---
+++ JS/script.js
@@ -0,0 +1,196 @@
+// console.log('Hello World.....');
+
+// console.log("Value of a: ", 5+5)
+
+
+
+// var x 
+// let x = 101;
+// const z = 102;
+
+
+// console.log("Value of x: ", x)
+
+
+
+// var x = 100;   //declaration , assignment
+
+
+// // var x = 200;    //redeclaration
+// x = 300     //reassignment
+
+// console.log(x)
+
+// let x = 200;
+
+// let x = 100;
+// // x = 300;
+
+// console.log(x)
+
+
+// const x = 1000;
+
+// // const x = 2000;
+// x = 3000;
+
+// console.log(x)
+
+
+
+// alert('Welcome')
+
+// const age = prompt('Enter your age: ')
+// console.log(age)
+
+
+// let employee = {
+//     fname: "Onkar",
+//     lname: "Bhawar",
+//     profile: "Full Stack Dev",
+//     empId: 101
+// }
+// console.log(employee)
+
+// const keys1 = Object.keys(employee)
+// console.log(keys1)
+
+
+// const arr = [11, 'Onkar']
+
+// console.log(arr)
+
+// function greet(){
+//     console.log('greet called')
+
+// }
+
+// greet()
+
+
+
+
+// let x = 100;  //global scope
+
+// function greet(){
+//     console.log('greet called')
+
+
+//     let y = 200
+//     console.log(x);
+//     console.log(y);
+// }
+
+
+// {
+//     let y = 111;
+//     console.log(y)
+// }
+
+
+// console.log(y)
+// greet()
+
+
+
+// var x = 111
+
+// function greet(){
+//     var x = 100;
+
+//     console.log('inside fn: ', x)
+// }
+
+
+// // console.log(x)
+// greet()
+
+
+
+// let x = [11, 22];
+// console.log(typeof(x))
+
+
+// let x = 20
+// let y = 3   ///3
+
+// console.log(x % y)  //2*2*2  %
+
+
+
+// let x = 100;
+
+// // console.log(++x)   //pre-increment
+// console.log(x++)   //post-increment
+// console.log(x)
+
+
+// let y = 100;
+// // console.log(--y)
+// console.log(y--)
+// console.log(y)
+
+
+// let x = 2
+// let y = 5
+// console.log(x)
+// x **= y         // x = x+y
+// console.log(x)
+
+
+
+
+// let x = 500
+// let y = 5000   //  '5'
+
+// console.log(x < y)
+
+
+// let x = 2;
+// let y = ''
+
+//ternary operator / conditional opr
+// part1 ? part2 : part3
+
+// condition ? true : false;
+
+// x == y  ? console.log('EQUAL') : console.log('NOT EQUAL')
+
+
+
+
+//logical opr
+
+//logical AND        &&
+
+// let x = 100
+// let y = 200
+// console.log((x < y) && (y==200) )
+// console.log((x < y) && (x==10) )
+// console.log((x > y) && (x==100) )
+// console.log((x > y) && (x==100) )
+
+//logical OR  ||
+
+// let x = 100;
+// let y = 200;
+
+// console.log((x < y) || (x==100) )
+// console.log((x < y) || (x==10) )
+// console.log((x > y) || (x==100) )
+// console.log((x > y) || (x==10) )
+
+//logical NOT   !
+
+// let x = 8;
+// console.log(!x)      
+
+let fname = 'Onkar';
+let lname = "Bhawar"
+let fullName = `My name is ${fname} ${lname}.`
+
+// console.log(fname.length)
+// console.log(fullName.toLowerCase())
+// console.log(fname.toUpperCase())
+console.log(fname.charAt(200))
+// console.log(fullName.indexOf('nam'))
